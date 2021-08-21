3099fa29bd0b990069269feccead3dc17612a946
Nagraj <nsiddesure@apple.com>
2021-08-21 00:51:48 -0500

    Added test cases to validate the repository model creation and ensure model properties are holding correct values

--- gitHubApp/gitHubAppTests/gitHubAppTests.m
+++ gitHubApp/gitHubAppTests/gitHubAppTests.m
@@ -6,6 +6,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "GHRepository.h"
 
 @interface gitHubAppTests : XCTestCase
 
@@ -31,6 +32,30 @@
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
+}
+
+- (id)JSONForResourceFile:(NSString *)resourceName {
+    NSString * fileName = [[NSBundle mainBundle] pathForResource:resourceName
+                                                         ofType:@"json"];
+    NSData * data = [NSData dataWithContentsOfFile:fileName];
+    return [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+}
+
+// Test case to ensure the GHRepository model object is getting initialized and properties are updated correctly with json dictionary
+- (void)testGHRepositoryModeInitialization {
+    id jsonObject = [self JSONForResourceFile:@"testRepository"];
+    NSArray * repositoryList = jsonObject;
+    
+    XCTAssertTrue(repositoryList.count == 1, "Incorrect repository list count");
+
+    for (NSDictionary * eachRepository in repositoryList) {
+        NSString * name = [eachRepository objectForKey:@"name"];
+        NSString * identifier = [eachRepository objectForKey:@"id"];
+
+        GHRepository * repository = [[GHRepository alloc] initWithDictionary:eachRepository];
+        XCTAssertEqualObjects(repository.name, name, @"Failure: The repository name is not matching, Expected: The repository name is matching");
+        XCTAssertEqualObjects(repository.identifier, identifier, @"Failure: The repository id is not matching, Expected: The repository id is matching");
+    }
 }
 
 @end
